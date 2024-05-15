3a83c04961f18130f93d8974dc385690bd436da1
Rishi <rishisingh3301@gmail.com>
2024-05-15 15:55:42 +0530

    deploy changes

---
+++ build/precache-manifest.13fa2d1284945db32e39c2668a8a34fd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d8d9c6cb255956f5a60491aebbabf5a",
+    "url": "/NoteSync/index.html"
+  },
+  {
+    "revision": "95e62f1f7ad4ba5683b2",
+    "url": "/NoteSync/static/js/2.ed06f336.chunk.js"
+  },
+  {
+    "revision": "92040bac2e04d752fdf1",
+    "url": "/NoteSync/static/js/main.ad1237a0.chunk.js"
+  },
+  {
+    "revision": "6880e9f196c502420cd5",
+    "url": "/NoteSync/static/js/runtime-main.91c501bd.js"
+  }
+]);
